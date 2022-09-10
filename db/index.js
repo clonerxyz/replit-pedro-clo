8c1a35ddaae09618f00289bafb07744a785de01d
VilosmZX <jravaell@gmail.com>
2022-09-10 22:24:16 +0700

    Tambahin fungsi storChatLog(msg)

---
+++ db/index.js
@@ -0,0 +1,21 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function storeChatLog(msg) {
+  const data = {
+    author: (await msg.getContact()),
+    msg: msg.body,
+    chat: (await msg.getChat()).name
+  };
+  await prisma.chatLog.create({
+    data: {
+      name: data.author?.pushname,
+      msg: data.msg,
+      number: data.author?.number,
+      group: data.chat,
+    }
+  });
+  io.sockets.emit('new_msg', data);
+}
+
+module.exports = { storeChatLog };
